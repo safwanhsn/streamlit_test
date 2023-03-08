6e3c12aaee87e24143dfc9389bf5c6fed3120a5c
sahossain <safone12@gmail.com>
2023-03-08 14:43:38 -0700

    t

--- obs_query.py
+++ obs_query.py
@@ -7,14 +7,14 @@ import sys
 import clr 
 # sys.path.append(r'C:Program Files (x86)PIPCAFPublicAssemblies4.0')    
 # clr.AddReference('OSIsoft.AFSDK') 
-sys.path.append(r'C:\Program Files (x86)\PIPC\AF\PublicAssemblies\4.0')    
-clr.AddReference('OSIsoft.AFSDK') 
-from OSIsoft.AF import *  
-from OSIsoft.AF.PI import *  
-from OSIsoft.AF.Asset import *  
-from OSIsoft.AF.Data import *  
-from OSIsoft.AF.Time import *  
-from OSIsoft.AF.UnitsOfMeasure import *
+# sys.path.append(r'C:\Program Files (x86)\PIPC\AF\PublicAssemblies\4.0')    
+# clr.AddReference('OSIsoft.AFSDK') 
+# from OSIsoft.AF import *  
+# from OSIsoft.AF.PI import *  
+# from OSIsoft.AF.Asset import *  
+# from OSIsoft.AF.Data import *  
+# from OSIsoft.AF.Time import *  
+# from OSIsoft.AF.UnitsOfMeasure import *
 
 import streamlit as st
 

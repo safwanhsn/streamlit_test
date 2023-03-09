1012cb93b0eed5b96ccee67b9f51ba7e5aa42e9d
sahossain <safone12@gmail.com>
2023-03-09 15:28:14 -0700

    data lake adls

--- app.py
+++ app.py
@@ -1,5 +1,74 @@
-import streamlit as st 
-import pandas as pd
+"""
+Libraries
+"""
 
-test = pd.read_csv('surveys.csv')
-st.write(test)
+#Streamlit components
+from st_aggrid import AgGrid
+import streamlit as st 
+
+#Computation
+import pandas as pd
+import numpy as np 
+import datetime as dt
+
+#Visualization
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import altair as alt
+from plotly.subplots import make_subplots
+
+#Loading Data
+from get_obs_data import download_file
+
+"""
+State Session Variables
+"""
+
+
+"""
+Page
+"""
+#Page Config
+st.set_page_config(page_title='OBS Well Analyzer')
+
+#Sidebar
+with st.sidebar:
+    st.title("OBS Well Analyzer")
+    st.caption("Dev Build")
+    
+    #Filter Section
+    # with st.expander("Filter"):
+    #     #init lists
+    #     #return obsDictionary
+    #     listProjects = obsDictionary.PROJECT.unique()
+    #     listPads = obsDictionary.SURFACE_PAD.unique()
+    #     #! List Patterns?
+    #     listParentWells = obsDictionary.WELL_PAIR.unique()
+    #     #! List Parameters
+        
+
+
+    #     project = st.multiselect('By Project', listProjects)
+
+    #     #! Insert Filter Function Here
+    #     surfacePad = st.multiselect('By Surface Pad', listPads)
+
+    #     parentWell = st.multiselect('By Parent Well-Pair', listParentWells)
+
+    #     availableParameter = st.multiselect('By Available Parameters', listParameters)
+
+    #     # wellType = st.multiselect('By Well Type', ('Observation', 'Vertical (WIP)')) #! DIP does not contain the metadata to do this yet I believe
+
+    #     #! I think I want to do the above filtering like a form - And then maybe query the logs or a presaved csv or something or like distinct wihtin logs or query all log and filter to make list idk
+
+    #     obsWell = st.selectbox('Observation Well', listObsWells)
+
+st.write(download_file())      
+
+
+
+
+
+
+=
---
+++ get_obs_data.py
@@ -0,0 +1,64 @@
+"""
+Libraries
+"""
+
+#Computation
+import pandas as pd
+import numpy as np 
+import datetime as dt
+
+#Azure
+import io
+import os, uuid, sys
+from azure.storage.filedatalake import DataLakeServiceClient
+from azure.core._match_conditions import MatchConditions
+from azure.storage.filedatalake._models import ContentSettings
+
+#! Can we get our OBS list through a SQL Query of wellbore description or a CSV table that will eventually become a SQL table
+#!  Using change function show metadata DF of selected OBS well
+
+"""
+Functions to Connect to ADLS Gen2
+"""
+def initialize_storage_account(storage_account_name, storage_account_key):
+    
+    try:  
+        global service_client
+
+        service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
+            "https", storage_account_name), credential=storage_account_key)
+        
+    
+    except Exception as e:
+        print(e)
+
+def download_file():
+    try:
+        initialize_storage_account('dipdevarmdlsuw2001', 'SkElaMTddmVejnmY6BMnaLlVggUZidga1Te432ifKpUtt9kcraGZusveeOWORhSZv+wXSLc7XcfiPrOEtF5L9A==')
+
+        file_system_client = service_client.get_file_system_client(file_system="gold")
+
+        directory_client = file_system_client.get_directory_client(r"dts\obs-well-mapping\mackay-river\parent-child\750-P01")
+
+        file_client = directory_client.get_file_client("part-00000-d8a674b9-1c40-4fd9-8264-e0f058026633-c000.snappy.parquet")
+
+        download = file_client.download_file()
+
+        downloaded_bytes = download.readall()
+
+        downloadedParquet = io.BytesIO(downloaded_bytes)
+
+        dfParquet = pd.read_parquet(downloadedParquet) #! Speed this up
+
+        return dfParquet
+
+
+    except Exception as e:
+     print(e)
+
+
+
+
+
+
+

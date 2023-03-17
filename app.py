fefc32a30f507286bbbd336182889f2c696da4f5
sahossain <safone12@gmail.com>
2023-03-16 19:34:21 -0600

    ASD

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ for col in st.session_state.traceSelectionMatrix.columns:
     
     toPlot = st.session_state.traceSelectionMatrix[col]
     for parameter in toPlot:
-        st.write(obs_property[(obs_property['COMMON_WELLNAME']==obsWell)&(obs_property['PROPERTY_SHORT_NAME']==parameter)]['SOURCE'].reset_index(drop=True).iloc[0])
+        st.write(obs_property[(obs_property['COMMON_WELLNAME']==obsWell)&(obs_property['PROPERTY_SHORT_NAME']==parameter)].reset_index(drop=True).SOURCE)
         try:
             parameterSource = obs_property[(obs_property['COMMON_WELLNAME']==obsWell)&(obs_property['PROPERTY_SHORT_NAME']==parameter)]['SOURCE'].reset_index(drop=True).iat[0]
 

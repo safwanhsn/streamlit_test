defb63e6af9fbf6948dfb35c5156528de84fbf39
sahossain <safone12@gmail.com>
2023-03-29 16:53:27 -0600

    Test env push

--- app.py
+++ app.py
@@ -211,22 +211,23 @@ with elements("dashboard"):
     # as first parameter, plus additional properties you can find in the GitHub links below.
 
     with dashboard.Grid(layout):
-        mui.Paper("First item", key="first_item")
+        # mui.Paper("First item", key="first_item")
+        st.plotly_chart(fig1, theme="streamlit", key="first_item")
         mui.Paper("Second item (cannot drag)", key="second_item")
         mui.Paper("Third item (cannot resize)", key="third_item")
 
     # If you want to retrieve updated layout values as the user move or resize dashboard items,
     # you can pass a callback to the onLayoutChange event parameter.
 
-    def handle_layout_change(updated_layout):
-        # You can save the layout in a file, or do anything you want with it.
-        # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
-        print(updated_layout)
+    # def handle_layout_change(updated_layout):
+    #     # You can save the layout in a file, or do anything you want with it.
+    #     # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
+    #     print(updated_layout)
 
-    with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
-        mui.Paper("First item", key="first_item")
-        mui.Paper("Second item (cannot drag)", key="second_item")
-        mui.Paper("Third item (cannot resize)", key="third_item")
+    # with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
+    #     mui.Paper("First item", key="first_item")
+    #     mui.Paper("Second item (cannot drag)", key="second_item")
+    #     mui.Paper("Third item (cannot resize)", key="third_item")
 
 
         
